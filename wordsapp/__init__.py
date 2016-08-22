170b8251c76d7a910a3cefa40fcf76b8b74feac9
Denis Karanja <denis.karanja@savannahinformatics.com>
2016-08-22 09:10:32 +0300

    Add a title case output format for the converted string.

--- wordsapp/__init__.py
+++ wordsapp/__init__.py
@@ -1,8 +1,7 @@
-from .base_wrapper import NumbersBaseWrapper
 from .number_to_words import NumbersToWord
 
 AUTHOR = "Master Yoda @yoda-yoda"
 VERSION = (0, 0, 1)
 
 
-__all__ = [NumbersBaseWrapper, NumbersToWord]
+__all__ = [NumbersToWord]
--- wordsapp/number_to_words.py
+++ wordsapp/number_to_words.py
@@ -9,19 +9,78 @@ For instance :
 """
 import math
 
-from to_words.base_wrapper import NumbersBaseWrapper
+
+class NumbersBaseWrapper(object):
+    """Base wrapper class."""
+
+    hyphen = '-'
+    conjunction = ' and '
+    separator = ', '
+    negative = 'negative '
+    decimal = ' point '
+    space = ' '
+
+    dictionary = {
+        0: 'zero',
+        1: 'one',
+        2: 'two',
+        3: 'three',
+        4: 'four',
+        5: 'five',
+        6: 'six',
+        7: 'seven',
+        8: 'eight',
+        9: 'nine',
+        10: 'ten',
+        11: 'eleven',
+        12: 'twelve',
+        13: 'thirteen',
+        14: 'fourteen',
+        15: 'fifteen',
+        16: 'sixteen',
+        17: 'seventeen',
+        18: 'eighteen',
+        19: 'nineteen',
+        20: 'twenty',
+        30: 'thirty',
+        40: 'fourty',
+        50: 'fifty',
+        60: 'sixty',
+        70: 'seventy',
+        80: 'eighty',
+        90: 'ninety',
+        100: 'hundred',
+        1000: 'thousand',
+        1000000: 'million',
+        1000000000: 'billion',
+        1000000000000: 'trillion',
+        1000000000000000: 'quadrillion',
+        1000000000000000000: 'quintillion'
+    }
 
 
 class NumbersToWord(NumbersBaseWrapper):
     """Convert Numbers to words."""
 
-    def to_words(self, number):
+    def title_case(self, string):
+        """Title case a string."""
+        try:
+            assert isinstance(string, str), ('Ensure you pass a string.')
+            words = string.split(" ")
+            new_string = ''
+            for word in words:
+                new_string += word.capitalize() + self.space
+            return new_string
+        except AssertionError as e:
+            print(e)
+
+    def number_to_words(self, number):
         """Convert a number into words."""
         if not isinstance(number, (int, float,)):
             return False
 
         if number < 0:
-            return self.negative + self.to_words(abs(number))
+            return self.negative + self.number_to_words(abs(number))
 
         string = fraction = None
 
@@ -49,21 +108,21 @@ class NumbersToWord(NumbersBaseWrapper):
                 self.dictionary[100]
             if remainder:
                 # Make a recursive call
-                string += self.conjunction + self.to_words(remainder)
+                string += self.conjunction + self.number_to_words(remainder)
         else:
             # Make a recursive call for the num_base_units
             log = math.floor(math.log(number, 1000))
             base_unit = math.pow(1000, log)
             num_base_units = int(number / base_unit)
             remainder = number % base_unit
-            string = self.to_words(num_base_units) + self.space + \
+            string = self.number_to_words(num_base_units) + self.space + \
                 self.dictionary[base_unit]
 
             if remainder:
                 # Make a recurssive call for the remainder
                 string += self.conjunction if remainder < 100 else \
                     self.separator
-                string += self.to_words(remainder)
+                string += self.number_to_words(remainder)
 
         if (fraction is not None) and isinstance(fraction, int):
             string += self.decimal
@@ -72,4 +131,7 @@ class NumbersToWord(NumbersBaseWrapper):
                 words += self.dictionary[int(each)] + self.space
             string += words
 
-        return string
+        return self.title_case(string.replace("  ", " "))
+
+obj = NumbersToWord()
+print(obj.number_to_words(82974.43))
